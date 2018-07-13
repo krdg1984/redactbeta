f16f0bc9d0c4ed36232e5be3a234a09b2f3f496a
davidz2018 <dzhomebiz@gmail.com>
2018-07-12 20:06:18 -0700

    updated the Formjs with the onChange function working

--- src/components/Card/Form.js
+++ src/components/Card/Form.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Form extends Component {
   constructor(props) {
     super(props);
+    this.handleChange.bind(this)
     this.state = {
                   loans: '',
                   creditCards: '',
@@ -14,31 +15,15 @@ class Form extends Component {
                   travel: '',
                   buyHome: '',
                   invest: ''
-                  };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+                  }
   }
 
-  handleChange(event) {
-    this.setState({
-                   loans: event.target.loans,
-                   creditCards: event.target.creditCards,
-                   utilities: event.target.utilities,
-                   rentMortgage: event.target.rentMortgage,
-                   transportation: event.target.transportation,
-                   debt: event.target.debt,
-                   activities: event.target.activities,
-                   travel: event.target.travel,
-                   buyHome: event.target.buyHome,
-                   invest: event.target.invest 
-                  
-                  });
+  handleChange(e, value) {
+    let change = {}
+    change[value] = e.target.value
+    this.setState(change)
   } 
-
-  handleSubmit(event) {
-    event.preventDefault();
-  }
+  
     render () {
         
      return (
@@ -46,7 +31,7 @@ class Form extends Component {
         <div className="field">
          
           <label>Monthly Income:</label>
-          <ul><input income={this.state.income} onChange={this.handleChange} name='income' value= ''></input></ul>
+          <ul><input type="income" onChange={(event)=>this.handleChange(event, "income")} value={this.state.income}></input></ul>
 
         </div>
 
@@ -54,15 +39,15 @@ class Form extends Component {
           
           <label>Monthly Expenses:</label>
          
-            <ul>Loans:</ul><ul><input loans={this.state.loans} onChange={this.handleChange} name='loans' value= '' ></input></ul>
+            <ul>Loans:</ul><ul><input type="loans" onChange={(event)=>this.handleChange(event, "loans")} value={this.state.loans} ></input></ul>
          
-            <ul>Credit Cards:</ul><ul><input creditCards={this.state.creditCards} onChange={this.handleChange} name='creditCards' value= '' ></input></ul>
+            <ul>Credit Cards:</ul><ul><input type="creditCards" onChange={(event)=>this.handleChange(event, "creditCards")} value={this.state.creditCards}></input></ul>
          
-            <ul>Utilities:</ul><ul><input utilities={this.state.utilities} onChange={this.handleChange} name='utilities' value= ''></input></ul>
+            <ul>Utilities:</ul><ul><input type="utilities" onChange={(event)=>this.handleChange(event, "utilities")} value={this.state.utilities} ></input></ul>
          
-            <ul>Rent/Mortgage:</ul><ul><input rentMortgage={this.state.rentMortgage} onChange={this.handleChange} name='rentMortgage' value= ''></input></ul>
+            <ul>Rent/Mortgage:</ul><ul><input type="rentMorgage" onChange={(event)=>this.handleChange(event, "rentMortgage")} value={this.state.rentMortgage}></input></ul>
          
-            <ul>Transportation:</ul><ul><input transportation={this.state.transportation} onChange={this.handleChange} name='transportation' value= ''></input></ul>
+            <ul>Transportation:</ul><ul><input type="transportation" onChange={(event)=>this.handleChange(event, "transportation")} value={this.state.transportation}></input></ul>
         
         </div>
         
@@ -70,15 +55,15 @@ class Form extends Component {
           
           <label>Goal:</label>
             
-            <ul>Pay off debt:</ul><ul><input debt={this.state.debt} onChange={this.handleChange} name='debt' value= ''></input></ul>
+            <ul>Pay off debt:</ul><ul><input type="debt" onChange={(event)=>this.handleChange(event, "debt")} value={this.state.debt}></input></ul>
             
-            <ul>Activities:</ul><ul><input activities={this.state.activities} onChange={this.handleChange} name='activities' value= ''></input></ul>
+            <ul>Activities:</ul><ul><input type="activities" onChange={(event)=>this.handleChange(event, "activities")} value={this.state.activities}></input></ul>
             
-            <ul>Travel:</ul><ul><input travel={this.state.travel} onChange={this.handleChange} name='travel' value= ''></input></ul>
+            <ul>Travel:</ul><ul><input type="travel" onChange={(event)=>this.handleChange(event, "travel")} value={this.state.travel}></input></ul>
             
-            <ul>Buy a home:</ul><ul><input buyHome={this.state.buyHome} onChange={this.handleChange} name='buyHome' value= ''></input></ul>
+            <ul>Buy a home:</ul><ul><input type="buyHome" onChange={(event)=>this.handleChange(event, "buyHome")} value={this.state.buyHome}></input></ul>
             
-            <ul>Investment Funds:</ul><ul><input invest={this.state.invest} onChange={this.handleChange} name= 'invest' value= ''></input></ul>
+            <ul>Investment Funds:</ul><ul><input type="invest" onChange={(event)=>this.handleChange(event, "invest")} value={this.state.invest}></input></ul>
        
         </div>
         
