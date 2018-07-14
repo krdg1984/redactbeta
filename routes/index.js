74ae25fc9180ca52542921c8ba9fa583c2ef75be
Travell Hector <travell.hector@gmail.com>
2018-07-14 11:06:48 -0700

    added models and routes

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var router = require('express').Router();
+var db = require('../models');
+var passport = require('passport');
+
+
+//GO TO REACT ***DONT KNOW WHERE TO DIRECT
+router.get('/', function(req, res) {
+      res.render('index')
+})
+
+
+
+//REGISTER NEW USER
+router.post('/register/user', function(req, res){
+      var firstName = req.body.firstname;
+      var lastName = req.body.lastname;
+      var email = req.body.email;
+      var password = req.body.password;
+
+      //POST NEW USER TO DATABASE
+      db.user.create(req.body).then(function(user) {
+            //AUTO SIGNIN NEW USER
+            req.login(user, function(err){
+                  console.log("inside req.login and it works, user.id: " + user.id);
+
+                  if (err) {
+                        console.log(err);
+                        return next(err); 
+                  };
+                  console.log('newUsers first name is: ' + req.user.firstName);
+                  //need to redirect to react to display form data (income, expenses, etc...)
+
+            })
+      
+      });
+      
+});
+
+//SIGNIN USER
+
+router.post('/signin', passport.authenticate('local', {
+      successRedirect: '/dashboard',
+      failureRedirect: '/user/signin'
+      })
+      // , function(req, res){
+      //       res.redirect('/dashboard')
+      // }
+);
+
+//SIGNOUT USER
+router.get('/logout', function(req, res){
+      req.logout();
+      res.redirect('/');
+});
+
+//REDIRECT IF USER NOT SIGNED IN
+router.use(function(req, res, next){
+      if (!req.user){
+            res.redirect('/user/signin')
+      }else{
+            return next();
+      };
+})
+
+
+module.exports = router
